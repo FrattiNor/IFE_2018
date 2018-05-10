b2ad71ec0cf52213a0687765c769b883928fa58d
FrattiNor <1270344396@qq.com>
2018-05-10 15:17:44 +0800

    add

--- css/No.8/js/myjs.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-    container: document.getElementById("animation2"),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://github.com/FrattiNor/IFE_2018/blob/master/css/No.8/json/data1.json'
-});
